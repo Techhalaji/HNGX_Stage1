431cc9bd916845cbf7faafd7379ee875d261785b
Techhalaji <114708081+Techhalaji@users.noreply.github.com>
2023-09-09 20:28:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Set the current day of the week
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    function updateTime() {
+        const currentDate = new Date();
+        const currentUTC = new Date(currentDate.toUTCString()); // Convert to UTC time
+        
+        const currentDayOfWeek = daysOfWeek[currentDate.getUTCDay()];
+        document.getElementById("dayOfWeek").textContent = currentDayOfWeek;
+
+        const currentHours = currentDate.getUTCHours();
+        const currentMinutes = currentDate.getUTCMinutes();
+        const currentSeconds = currentDate.getUTCSeconds();
+
+        document.getElementById("currentTime").textContent = `Current Time (Local): ${currentDate.toLocaleTimeString()}`;
+        document.getElementById("utcTime").textContent = `UTC Time (ms): ${currentUTC.getTime()}`;
+    }
+
+    // Initial call to updateTime
+    updateTime();
+
+    // Update the time every second
+    setInterval(updateTime, 1000);
+});
